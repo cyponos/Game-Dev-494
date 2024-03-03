d62d559905124827f5f77b3c8b1e17fcaa5258a0
cyponos <98052408+cyponos@users.noreply.github.com>
2024-03-03 14:03:18 -0600

    Movementish
    Movement clonky, you fly rn but you can move and jump

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -13,6 +13,20 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("w"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(2, 0, 0);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("s"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(-2, 0, 0);
+        }
+
     }
 }
